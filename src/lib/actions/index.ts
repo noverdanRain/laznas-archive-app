73e0cc228baead08a85606d04ca1287205c79267
harundarat <harundarat@gmail.com>
2025-08-05 14:09:04 +0700

    feat: implement updateStaffById function and add corresponding tests

--- src/lib/actions/index.ts
+++ src/lib/actions/index.ts
@@ -1,41 +1,47 @@
 "use server";
 
 // Query
-import { getUserSession, } from "./query/user-session";
-import { getDivisions, } from "./query/divisions";
-import { getAllStaff, getStaffById, } from "./query/staff";
-import { getDirectories, getTotalDocsInDirectory, } from "./query/directories";
-import { getDocumentTypes, } from "./query/document-type";
-import { getPinataPresignedUrl, } from "./query/pinata-presigned-url";
+import { getUserSession } from "./query/user-session";
+import { getDivisions } from "./query/divisions";
+import { getAllStaff, getStaffById } from "./query/staff";
+import { getDirectories, getTotalDocsInDirectory } from "./query/directories";
+import { getDocumentTypes } from "./query/document-type";
+import { getPinataPresignedUrl } from "./query/pinata-presigned-url";
 import { getAllDocuments } from "./query/documents";
 
 export {
-    getUserSession,
-    getDivisions,
-    getAllStaff,
-    getStaffById,
-    getDirectories,
-    getTotalDocsInDirectory,
-    getDocumentTypes,
-    getPinataPresignedUrl,
-    getAllDocuments,
+  getUserSession,
+  getDivisions,
+  getAllStaff,
+  getStaffById,
+  getDirectories,
+  getTotalDocsInDirectory,
+  getDocumentTypes,
+  getPinataPresignedUrl,
+  getAllDocuments,
 };
 
 // =================================================================================
 
 // Mutation
-import { addStaff, disableStaff, enableStaff, } from "./mutation/staff";
-import { removeUserSession, createUserSession, } from "./mutation/user-session";
-import { addDirectory, } from "./mutation/directories";
+import {
+  addStaff,
+  disableStaff,
+  enableStaff,
+  updateStaffById,
+} from "./mutation/staff";
+import { removeUserSession, createUserSession } from "./mutation/user-session";
+import { addDirectory } from "./mutation/directories";
 import { addDocument, addDocumentHistory } from "./mutation/documents";
 
 export {
-    addStaff,
-    disableStaff,
-    enableStaff,
-    createUserSession,
-    removeUserSession,
-    addDirectory,
-    addDocument,
-    addDocumentHistory,
+  addStaff,
+  disableStaff,
+  enableStaff,
+  createUserSession,
+  removeUserSession,
+  addDirectory,
+  addDocument,
+  addDocumentHistory,
+  updateStaffById,
 };
--- src/lib/actions/mutation/staff.ts
+++ src/lib/actions/mutation/staff.ts
@@ -8,86 +8,146 @@ import { randomUUID } from "crypto";
 import { throwActionError } from "../helpers";
 
 export interface IAddStaffParams {
-    username: string;
-    password: string;
-    divisionId: string;
+  username: string;
+  password: string;
+  divisionId: string;
 }
 
+type UpdateStaffByIdParams = {
+  id: string;
+  username?: string;
+  password?: string;
+  divisionId?: string;
+};
+
 async function addStaff(
-    params: IAddStaffParams
+  params: IAddStaffParams
 ): Promise<MutateActionsReturnType> {
-    const { username, password, divisionId } = params;
-    try {
-        const [existingUser] = await db
-            .select()
-            .from(users)
-            .where(eq(users.username, username));
-        if (existingUser?.username) {
-            return {
-                isSuccess: false,
-                isRejected: true,
-                reject: {
-                    message: "Username sudah dipakai.",
-                },
-            };
-        }
-        const generatedUid = randomUUID();
-        const hashedPassword = await bcrypt.hash(password, 10);
-        await db.insert(users).values({
-            id: generatedUid,
-            username: username.toLocaleLowerCase(),
-            password: hashedPassword,
-            divisionId,
-        });
-        revalidateTag("all-staff");
-        return {
-            isSuccess: true,
-            isRejected: false,
-        };
-    } catch (error) {
-        console.error("Error adding staff:", error);
-        throwActionError(error);
+  const { username, password, divisionId } = params;
+  try {
+    const [existingUser] = await db
+      .select()
+      .from(users)
+      .where(eq(users.username, username));
+    if (existingUser?.username) {
+      return {
+        isSuccess: false,
+        isRejected: true,
+        reject: {
+          message: "Username sudah dipakai.",
+        },
+      };
     }
+    const generatedUid = randomUUID();
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await db.insert(users).values({
+      id: generatedUid,
+      username: username.toLocaleLowerCase(),
+      password: hashedPassword,
+      divisionId,
+    });
+    revalidateTag("all-staff");
+    return {
+      isSuccess: true,
+      isRejected: false,
+    };
+  } catch (error) {
+    console.error("Error adding staff:", error);
+    throwActionError(error);
+  }
 }
 
 async function disableStaff(id: string): Promise<MutateActionsReturnType> {
-    try {
-        await db
-            .update(users)
-            .set({ isDisabled: true })
-            .where(eq(users.id, id));
+  try {
+    await db.update(users).set({ isDisabled: true }).where(eq(users.id, id));
 
-        revalidateTag(`staff-${id}`);
-        revalidateTag("all-staff");
+    revalidateTag(`staff-${id}`);
+    revalidateTag("all-staff");
 
-        return {
-            isSuccess: true,
-            isRejected: false,
-        };
-    } catch (error) {
-        console.error("Error disabling staff:", error);
-        throwActionError(error);
-    }
+    return {
+      isSuccess: true,
+      isRejected: false,
+    };
+  } catch (error) {
+    console.error("Error disabling staff:", error);
+    throwActionError(error);
+  }
 }
 
 async function enableStaff(id: string): Promise<MutateActionsReturnType> {
-    try {
-        await db
-            .update(users)
-            .set({ isDisabled: false })
-            .where(eq(users.id, id));
+  try {
+    await db.update(users).set({ isDisabled: false }).where(eq(users.id, id));
 
-        revalidateTag(`staff-${id}`);
-        revalidateTag("all-staff");
+    revalidateTag(`staff-${id}`);
+    revalidateTag("all-staff");
 
-        return {
-            isSuccess: true,
-            isRejected: false,
-        };
-    } catch (error) {
-        console.error("Error enabling staff:", error);
-        throwActionError(error);
+    return {
+      isSuccess: true,
+      isRejected: false,
+    };
+  } catch (error) {
+    console.error("Error enabling staff:", error);
+    throwActionError(error);
+  }
+}
+async function updateStaffById(
+  params: UpdateStaffByIdParams
+): Promise<MutateActionsReturnType> {
+  try {
+    const { id, username, password, divisionId } = params;
+
+    if (!id) {
+      return {
+        isRejected: true,
+        reject: {
+          message: "ID staff tidak boleh kosong.",
+        },
+      };
     }
+
+    const [existingUser] = await db
+      .select()
+      .from(users)
+      .where(eq(users.id, id))
+      .limit(1);
+
+    if (!existingUser) {
+      return {
+        isRejected: true,
+        reject: {
+          message: `Staff dengan id ${id} tidak ditemukan.`,
+        },
+      };
+    }
+
+    const updateData: Record<string, string> = {};
+    if (username) {
+      updateData.username = username;
+    }
+    if (divisionId) {
+      updateData.divisionId = divisionId;
+    }
+    if (password) {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      updateData.password = hashedPassword;
+    }
+    if (Object.keys(updateData).length === 0) {
+      return {
+        isRejected: true,
+        reject: {
+          message: "Tidak ada data yang perlu diperbarui.",
+        },
+      };
+    }
+
+    await db.update(users).set(updateData).where(eq(users.id, id));
+    return {
+      isSuccess: true,
+    };
+  } catch (error) {
+    console.error("Error select staff by id: ", error);
+    throwActionError(error);
+  }
 }
 
-export { addStaff, disableStaff, enableStaff };
+export { addStaff, disableStaff, enableStaff, updateStaffById };
